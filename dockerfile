75c5b935f482ac757072c292bd78315742204802
Maciej Topolewski <maciej_topolewski@epam.com>
2021-10-08 13:27:43 +0200

    Fix Docker, update setting and requirements

--- dockerfile
+++ dockerfile
@@ -1,8 +1,9 @@
 # syntax=docker/dockerfile:1
 FROM python:3.10.0-slim-buster
 ENV PYTHONUNBUFFERED 1
-WORKDIR /code
+WORKDIR /code/forum_scraper
 COPY requirements.txt /
 RUN pip install -r /requirements.txt
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
 # COPY . /code/ -- for local development we mounted it explicitely so Django
 # can reload on code changes without rebuilding Docker
--- forum_scraper/forum_scraper/settings.py
+++ forum_scraper/forum_scraper/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SECRET_KEY = config("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
