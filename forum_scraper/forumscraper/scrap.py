f97c57d4ba4412eb6471f8fa7febc2f5b301e28a
Maciej Topolewski <maciej.topolewski@gmail.com>
2021-10-11 14:31:55 +0200

    Add typehints, atomic methods

--- forum_scraper/forumscraper/scrap.py
+++ forum_scraper/forumscraper/scrap.py
@@ -4,17 +4,12 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_random_user_agent(user_agent_list_file):
-    user_agent = random.choice(open(user_agent_list_file).readlines())
-    return user_agent.strip()
-
-
 class Scraper:
-    def __init__(self, url):
+    def __init__(self, url: str):
         self.url = url
         self.paragraphs = []
         self.headers = {
-            "User-Agent": get_random_user_agent("user-agent-list.txt"),
+            "User-Agent": self.get_random_user_agent("user-agent-list.txt"),
             "Accept-Language": "en-gb",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
             "Accept-Encoding": "gzip, deflate, br",
@@ -27,21 +22,40 @@ class Scraper:
         # }
 
     @staticmethod
-    def list_to_string(bs4_content):
-        str1 = " "
-        return str1.join(bs4_content)
+    def get_random_user_agent(user_agent_list_file: str) -> str:
+        user_agent = random.choice(open(user_agent_list_file).readlines())
+        return user_agent.strip()
 
-    def get_subpages(self, class_type, class_name=None, recursive=None):
-        request = requests.get(self.url, headers=self.headers)  # for IP read add: stream=True
+    def get_response(self) -> requests.Response:
+        response = requests.get(
+            self.url, headers=self.headers
+        )  # for IP read add: stream=True
         # for IP read: print(request.raw._connection.sock.getsockname())
-        soup = BeautifulSoup(request.text, "html.parser")
+        return response
+
+    def get_subpages_from_request(
+        self, response: requests.Response, class_type: str, class_name: str = None
+    ) -> list:
+        soup = BeautifulSoup(response.text, "html.parser")
         div = soup(class_type, class_name)
         for elem in div:
             self.paragraphs.append(elem)
-        for paragraph in self.paragraphs:
-            print(paragraph.get_text(), ":   ", paragraph.get("href"))
+        # for paragraph in self.paragraphs:
+        #     print(paragraph.get_text(), ":   ", paragraph.get("href"))
+        return self.paragraphs
+
+    def get_tittles_and_links(self) -> dict:
+        return {paragraph.get_text(): paragraph.get("href") for paragraph in self.paragraphs}
+
+    @staticmethod
+    def list_to_string(bs4_content: BeautifulSoup) -> str:
+        str1 = " "
+        return str1.join(bs4_content)
 
 
 # example: Saab forum
 scraper = Scraper("http://saabotage.pl/")
-scraper.get_subpages("a", "forumlink")
+response = scraper.get_response()
+scraper.get_subpages_from_request(response, "a", "forumlink")
+d = scraper.get_tittles_and_links()
+print(d)
